8e3fb990e361f4c3572586cc34c4fbbe1431f464
cosmina2902 <cosmina.recea@student.upt.ro>
2024-04-28 03:41:13 +0300

    Configure the frontend for the application

---
+++ ems-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'> 
+            <span>Have a nice day!</span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
